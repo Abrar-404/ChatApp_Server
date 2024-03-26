68748d7efaacd4cf17fc894925e86b153f8b9cb8
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-26 13:09:51 +0600

    added user schema

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -20,26 +20,7 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-const userSchema = mongoose.schema({
-  fullName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  token: {
-    type: String,
-  },
-});
-
-const Users = mongoose.model('Users', userSchema);
+const users = require('./User')
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eted0lc.mongodb.net/?retryWrites=true&w=majority`;
 
