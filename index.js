673c94cf7eb585e9d628de10764098b5aae4658f
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-26 12:34:11 +0600

    adding id collection

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ async function run() {
       res.send(result);
     });
 
+    
+    app.get('/featureCards/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await featureCardCollection.findOne(query);
+      res.send(result);
+    });
+
     await client.db('admin').command({ ping: 1 });
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
